86a96f2a07b7d5ae34680945bf90af2adb35d6b7
Bertrik Sikken <bertrik@sikken.nl>
2022-07-19 00:33:12 +0200

    Add all PSR types.

--- energymix-server/src/main/java/nl/bertriksikken/entsoe/EPsrType.java
+++ energymix-server/src/main/java/nl/bertriksikken/entsoe/EPsrType.java
@@ -2,10 +2,35 @@ package nl.bertriksikken.entsoe;
 
 public enum EPsrType {
 
+    MIXED("A03"), //
     GENERATION("A04"), //
+    LOAD("A05"), //
+
+    BIOMASS("B01"), //
+    FOSSIL_BROWN_COAL("B02"), //
+    FOSSIL_COAL_DERIVED_GAS("B03"), //
+    FOSSIL_GAS("B04"), //
+    FOSSIL_HARD_COAL("B05"), //
+    FOSSIL_OIL("B06"), //
+    FOSSIL_OIL_SHALE("B07"), //
+    FOSSIL_PEAT("B08"), //
+    GEOTHERMAL("B09"), //
+    HYDRO_PUMPED_STORAGE("B10"), //
+    HYDRO_RIVER_POUNDAGE("B11"), //
+    HYDRO_RESERVOIR("B12"), //
+    MARINE("B13"), //
+    NUCLEAR("B14"), //
+    RENEWABLE_OTHER("B15"), //
     SOLAR("B16"), //
+    WASTE("B17"), //
     WIND_ONSHORE("B18"), //
-    WIND_OFFSHORE("B19"); //
+    WIND_OFFSHORE("B19"), //
+    OTHER("B20"), //
+
+    AC_LINK("B21"), //
+    DC_LINK("B22"), //
+    SUBSTATION("B23"), //
+    TRANSFORMER("B24"); //
 
     private final String code;
 
---
+++ energymix-server/src/test/java/nl/bertriksikken/entsoe/EPsrTypeTest.java
@@ -0,0 +1,22 @@
+package nl.bertriksikken.entsoe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class EPsrTypeTest {
+
+    /**
+     * Verifies that all PSR types have a unique code.
+     */
+    @Test
+    public void testUniqueCode() {
+        Map<String, EPsrType> codes = new HashMap<>();
+        for (EPsrType psrType : EPsrType.values()) {
+            Assert.assertNull(codes.put(psrType.getCode(), psrType));
+        }
+    }
+    
+}
