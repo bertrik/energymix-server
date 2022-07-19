4c024c4e7bbf59f99b75dd970a14afb398a442bf
Bertrik Sikken <bertrik@sikken.nl>
2022-07-19 22:50:31 +0200

    Rename actual.xml

--- energymix-server/src/test/java/nl/bertriksikken/entsoe/EntsoeResponseTest.java
+++ energymix-server/src/test/java/nl/bertriksikken/entsoe/EntsoeResponseTest.java
@@ -11,7 +11,7 @@ public final class EntsoeResponseTest {
 
     @Test
     public void testDeserialize() throws IOException {
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("actual.xml");
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("A75_actualgeneration.xml");
         XmlMapper mapper = new XmlMapper();
         EntsoeResponse document = mapper.readValue(is, EntsoeResponse.class);
         System.out.println(document);
