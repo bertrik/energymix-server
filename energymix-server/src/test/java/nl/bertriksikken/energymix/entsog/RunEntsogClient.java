f9ab729da229ee275b12057d952b2b0cdfa8f406
Bertrik Sikken <bertrik@sikken.nl>
2023-05-13 00:06:56 +0200

    Update entsog test runner.

--- energymix-server/src/test/java/nl/bertriksikken/energymix/entsog/RunEntsogClient.java
+++ energymix-server/src/test/java/nl/bertriksikken/energymix/entsog/RunEntsogClient.java
@@ -18,11 +18,15 @@ import nl.bertriksikken.energymix.entsoe.RunEntsoeClient;
 import nl.bertriksikken.entsog.EIndicator;
 import nl.bertriksikken.entsog.EOperatorKey;
 import nl.bertriksikken.entsog.EPeriodType;
+import nl.bertriksikken.entsog.EntsogAggregatedData;
 import nl.bertriksikken.entsog.EntsogRequest;
+import nl.bertriksikken.naturalgas.GasFlows;
+import nl.bertriksikken.naturalgas.GasFlowsFactory;
 
 public final class RunEntsogClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(RunEntsoeClient.class);
+    private static final ZoneId ZONE = ZoneId.of("Europe/Amsterdam");
 
     public static void main(String[] args) throws IOException {
         RunEntsogClient test = new RunEntsogClient();
@@ -30,20 +34,27 @@ public final class RunEntsogClient {
         ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
         EntsogClient client = EntsogClient.create(config, objectMapper);
 
-        test.fetchFlows(client, "entsog_aggregated_physical_flow.json");
+        String json = test.fetchFlows(client, "entsog_aggregated_physical_flow.json");
         LOG.info("done");
+
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+        EntsogAggregatedData aggregatedData = mapper.readValue(json, EntsogAggregatedData.class);
+        GasFlowsFactory factory = new GasFlowsFactory(ZONE);
+        GasFlows gasFlows = factory.build(aggregatedData);
+        String gasFlowsJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(gasFlows);
+        LOG.info("{}", gasFlowsJson);
     }
 
-    private void fetchFlows(EntsogClient client, String fileName) throws IOException {
-        ZoneId zoneId = ZoneId.of("Europe/Amsterdam");
+    private String fetchFlows(EntsogClient client, String fileName) throws IOException {
         EntsogRequest request = new EntsogRequest(EOperatorKey.GTS, EIndicator.PHYSICAL_FLOW);
-        ZonedDateTime endTime = ZonedDateTime.now(zoneId).truncatedTo(ChronoUnit.DAYS);
+        ZonedDateTime endTime = ZonedDateTime.now(ZONE).truncatedTo(ChronoUnit.DAYS);
         ZonedDateTime startTime = endTime.minusDays(1);
-        request.setPeriod(zoneId, EPeriodType.DAY, startTime.toInstant(), endTime.toInstant());
+        request.setPeriod(ZONE, EPeriodType.DAY, startTime.toInstant(), endTime.toInstant());
         String json = client.getRawDocument(request.getParams());
         try (Writer writer = new FileWriter(new File(fileName), StandardCharsets.UTF_8)) {
             writer.write(json);
         }
+        return json;
     }
 
 }
